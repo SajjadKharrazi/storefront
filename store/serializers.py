6a1904f95a03156c9ab39dba1747a76585862b96
John Doe <sajjadkharrazi@gmail.com>
2024-11-29 22:06:33 +0330

    first initialize

---
+++ store/serializers.py
@@ -0,0 +1,22 @@
+from decimal import Decimal
+from rest_framework import serializers
+from .models import Product, Collection
+
+
+class CollectionSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Collection
+        fields = ['id', 'title']
+
+    # products_count = serializers.IntegerField()
+
+class ProductSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Product
+        fields = ['id', 'title', 'unit_price', 'price_with_tax', 'collection']
+
+    collection = CollectionSerializer()
+    price_with_tax = serializers.SerializerMethodField('calculate_tax_rate')
+
+    def calculate_tax_rate(self, obj: Product):
+        return obj.unit_price * Decimal(1.1)
---
+++ store/urls.py
@@ -0,0 +1,10 @@
+from django.urls import path
+from . import views
+
+# URLConf
+urlpatterns = [
+    path('products/', views.ProductList.as_view()),
+    path('products/<int:id>/', views.product_detail),
+    # path('collections/', views.collection_list),
+    # path('collections/<int:id>/', views.product_detail),
+]
